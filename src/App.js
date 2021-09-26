c869b54e3967cda5318511e5e1347ff9c996d308
Albert Tian <atian@berkeley.edu>
2021-09-26 03:42:00 -0700

    content: per frame detection

--- src/App.js
+++ src/App.js
@@ -1,22 +1,101 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect } from "react"
 import './App.css';
+import * as tf from "@tensorflow/tfjs-core"
+import "@tensorflow/tfjs-backend-webgl"
+import "@tensorflow/tfjs-backend-wasm"
+import * as poseDetection from "@tensorflow-models/pose-detection"
+import Webcam from "react-webcam"
+
+const videoDim = {
+  width: 640,
+  height: 480
+}
+
+let rafId
+let camera, detector
 
 function App() {
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  async function loadMoveNet() {
+    setInterval(() => {
+      detect(detector)
+    }, 100)
+  }
+
+  function setupCamera() {
+    camera = webcamRef.current
+  }
+
+  async function setupDetector() {
+    const model = poseDetection.SupportedModels.MoveNet
+    const detectorConfig = {
+      modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING,
+      minPoseScore: 0.2,
+      enableTracking: true
+    }
+    detector = await poseDetection.createDetector(model, detectorConfig)
+  }
+
+
+  async function detect(detector) {
+    if (typeof camera === "undefined" || camera === null) return
+    if (camera.video.readyState !== 4) return
+    if (!detector) return
+
+    const video = camera.video
+    const videoWidth = video.videoWidth
+    const videoHeight = video.videoHeight
+
+    camera.video.width = videoWidth
+    camera.video.height = videoHeight
+
+    const poses = await detector.estimatePoses(video)
+    console.log(poses);
+  }
+
+  async function renderResult() {
+    await detect(detector)
+  }
+
+  async function renderPrediction() {
+    await renderResult()
+    rafId = requestAnimationFrame(renderPrediction)
+  }
+
+  async function run() {
+    setupCamera()
+    await setupDetector()
+    renderPrediction()
+  }
+
+  useEffect(() => {
+    run()
+  }, [webcamRef])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ position: 'relative' }}>
+          <Webcam
+            ref={webcamRef}
+            style={{
+              width: videoDim.width,
+              height: videoDim.height,
+            }}
+          />
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: 'absolute',
+              width: videoDim.width,
+              height: videoDim.height,
+              left: 0,
+              top: 0,
+            }} />
+        </div>
       </header>
     </div>
   );
