74cccfa60a7736f9076ada6a99338d86b23626e0
Albert Tian <atian@berkeley.edu>
2021-11-06 15:27:04 -0700

    content: beginning S3 integration

--- backend/heroku-test/server.js
+++ backend/heroku-test/server.js
@@ -2,7 +2,6 @@ const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
 
 const players = require('./data.json')
 
@@ -35,6 +34,24 @@ app.get('/players', (req, res) => {
     res.send(players)
 })
 
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-west-2'});
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Call S3 to list the buckets
+s3.listBuckets(function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.Buckets);
+  }
+});
+
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
 })
+
