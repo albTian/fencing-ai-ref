f430b891e239d3628a758cf2efc03eb5f8c3546d
Albert Tian <atian@berkeley.edu>
2021-11-06 14:05:14 -0700

    misc: yarn cleanup

--- backend/heroku-test/server.js
+++ backend/heroku-test/server.js
@@ -2,6 +2,7 @@ const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded());
 
 const players = require('./data.json')
 
