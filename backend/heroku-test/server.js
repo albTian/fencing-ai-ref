ca46f50b2f98262cd0fb7eea089cb43e7b8a17dd
Albert Tian <atian@berkeley.edu>
2021-11-06 13:19:32 -0700

    content: pushing to heroku with subtree

--- backend/heroku-test/server.js
+++ backend/heroku-test/server.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const app = express()
-const importData = require('./data.json')
+app.use(express.json());
+
+const players = require('./data.json')
 let port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.get('/players/:id', (req, res) => {
+    const player = players.find(p => p.id === parseInt(req.params.id));
+    if (player) {
+        res.send(player)
+    } else {
+        res.send("player not found...")
+    }
+})
+
+app.post('/players', (req, res) => {
+    const newPlayer = {
+        id: players.length + 1,
+        name: req.body.name,
+        weapon: req.body.weapon,
+        rating: req.body.rating,
+    }
+
+    players.push(newPlayer)
+    res.send(newPlayer)
+})
+
 app.get('/players', (req, res) => {
-    res.send(importData)
+    res.send(players)
 })
 
 app.listen(port, () => {
