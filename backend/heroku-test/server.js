de1c64ab5ca7db68ec5fa94af6d5f9ceb3a400f7
Albert Tian <atian@berkeley.edu>
2021-11-07 20:02:31 -0800

    content: trying vercel autodeploy and removed '/' from cors path

--- backend/heroku-test/server.js
+++ backend/heroku-test/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://fencing-ai-ref.vercel.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://fencing-ai-ref.vercel.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
