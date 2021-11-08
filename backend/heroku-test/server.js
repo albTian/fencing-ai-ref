b7b2908019bcd9425c5fabaefe20eba6630adae1
Albert Tian <atian@berkeley.edu>
2021-11-07 20:28:16 -0800

    misc: fiddling with CORS

--- backend/heroku-test/server.js
+++ backend/heroku-test/server.js
@@ -1,15 +1,13 @@
 const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
-app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, https://fencing-ai-ref.vercel.app");
-    res.setHeader("Access-Control-Allow-Origin", "https://fencing-ai-ref.vercel.app"); // update to match the domain you will make the request from
-    res.setHeader("Access-Control-Request-Headers", "https://fencing-ai-ref.vercel.app")
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Origin", "https://fencing-ai-ref.vercel.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(express.json());
 
 const players = require('./data.json')
 
--- frontend/src/components/RequestTester.jsx
+++ frontend/src/components/RequestTester.jsx
@@ -2,23 +2,19 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 function RequestTester() {
-  //   const instance = axios.create({
-  //     baseURL: "https://fencing-ai-heroku.herokuapp.com/",
-  //     timeout: 1000,
-  //   });
-  const instance = axios.create({
-    baseURL: "https://fencing-ai-heroku.herokuapp.com",
-    withCredentials: false,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    },
-  });
+  // const instance = axios.create({
+  //   baseURL: "https://fencing-ai-heroku.herokuapp.com",
+  //   withCredentials: false,
+  //   headers: {
+  //     "Access-Control-Allow-Origin": "*",
+  //     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  //   },
+  // });
 
   useEffect(() => {
     console.log("useEffect running...");
-    instance
-      .get("/players")
+    axios
+      .get("https://fencing-ai-heroku.herokuapp.com/players")
       .then((res) => {
         const data = res.data;
         console.log(data);
