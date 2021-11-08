fd5b46dea6f6702d8a65f7db90c325157efdd191
Albert Tian <atian@berkeley.edu>
2021-11-07 20:35:57 -0800

    content: cors working...?

--- frontend/src/components/RequestTester.jsx
+++ frontend/src/components/RequestTester.jsx
@@ -2,19 +2,14 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 function RequestTester() {
-  // const instance = axios.create({
-  //   baseURL: "https://fencing-ai-heroku.herokuapp.com",
-  //   withCredentials: false,
-  //   headers: {
-  //     "Access-Control-Allow-Origin": "*",
-  //     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  //   },
-  // });
+  const instance = axios.create({
+    baseURL: "https://fencing-ai-heroku.herokuapp.com",
+  });
 
   useEffect(() => {
     console.log("useEffect running...");
-    axios
-      .get("https://fencing-ai-heroku.herokuapp.com/players")
+    instance
+      .get("/players")
       .then((res) => {
         const data = res.data;
         console.log(data);
