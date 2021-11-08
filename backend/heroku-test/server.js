b0e700c364b2133d31c1ff67c9818ca96fabff0a
Albert Tian <atian@berkeley.edu>
2021-11-07 19:49:52 -0800

    content: added middleware for CORS, still testing...

--- backend/heroku-test/server.js
+++ backend/heroku-test/server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://fencing-ai-ref.vercel.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const players = require('./data.json')
 
 app.get('/', (req, res) => {
@@ -37,18 +43,18 @@ app.get('/players', (req, res) => {
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'us-east-1'});
+AWS.config.update({ region: 'us-east-1' });
 
 // Create S3 service object
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
+s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
 // Call S3 to list the buckets
-s3.listBuckets(function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.Buckets);
-  }
+s3.listBuckets(function (err, data) {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("Success", data.Buckets);
+    }
 });
 
 app.listen(PORT, () => {
