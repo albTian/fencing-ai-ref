06cc52b259cf4607afd4af916d0224a259a8af2c
Albert Tian <atian@berkeley.edu>
2021-11-07 20:13:13 -0800

    misc: fiddling with CORS

--- backend/heroku-test/server.js
+++ backend/heroku-test/server.js
@@ -5,6 +5,8 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://fencing-ai-ref.vercel.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Request-Headers", "https://fencing-ai-ref.vercel.app")
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -12,7 +14,7 @@ app.use(function (req, res, next) {
 const players = require('./data.json')
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World! I am changing')
 })
 
 app.get('/players/:id', (req, res) => {
