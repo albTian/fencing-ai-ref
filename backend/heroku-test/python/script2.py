b69d9847cf42dbbb45bf71303f95b232d2b0f469
Albert Tian <atian@berkeley.edu>
2021-11-13 18:28:20 -0800

    content: testing if pl works...

--- backend/heroku-test/python/script2.py
+++ backend/heroku-test/python/script2.py
@@ -4,7 +4,7 @@ import sys
 pl_url = "https://www.youtube.com/playlist?list=PL_pQQho0KExyLDnVl-JC6VzX3mOug0_sg"
 pl = Playlist(pl_url)
 
-print('#Hello from python#')
+print('#Hello from python#', pl)
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
 print("HELLO FROM FOLDER KEKEKEKE")
