e776f5ccd30842fa14aaa0c3259c02029ec060da
Albert Tian <atian@berkeley.edu>
2021-11-13 21:11:53 -0800

    content: experimenting with download video sript...

--- backend/heroku-test/python/script2.py
+++ backend/heroku-test/python/script2.py
@@ -1,10 +1,11 @@
-from pytube import Playlist, YouTube
 import sys
+import download_vids
 
-pl_url = "https://www.youtube.com/playlist?list=PL_pQQho0KExyLDnVl-JC6VzX3mOug0_sg"
-pl = Playlist(pl_url)
-
-print('#Hello from python#', pl)
+print('#Hello from python#')
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
 print("HELLO FROM FOLDER KEKEKEKE")
+
+pl_url = 'https://www.youtube.com/playlist?list=PL_pQQho0KExyLDnVl-JC6VzX3mOug0_sg'
+tournament_name = 'budapest-2019-wc'
+download_vids.main(pl_url, tournament_name)
