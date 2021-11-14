ea8032cf189929addcd957fa02e8f848cd36869e
Albert Tian <atian@berkeley.edu>
2021-11-13 17:54:46 -0800

    misc: heroku deployment

--- backend/heroku-test/python/script2.py
+++ backend/heroku-test/python/script2.py
@@ -1,10 +1,10 @@
-from pytube import Playlist, YouTube
+# from pytube import Playlist, YouTube
 import sys
 
-pl_url = "https://www.youtube.com/playlist?list=PL_pQQho0KExyLDnVl-JC6VzX3mOug0_sg"
-pl = Playlist(pl_url)
+# pl_url = "https://www.youtube.com/playlist?list=PL_pQQho0KExyLDnVl-JC6VzX3mOug0_sg"
+# pl = Playlist(pl_url)
 
-print('#Hello from python#', pl)
+print('#Hello from python#')
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
 print("HELLO FROM FOLDER KEKEKEKE")
