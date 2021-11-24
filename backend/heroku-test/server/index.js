b1427f06fe25ebfb7c19289a3c3eacc27ac8a082
Albert Tian <atian@berkeley.edu>
2021-11-24 11:03:31 -0800

    content: correct AWS bucket linked up localy

--- backend/heroku-test/server/index.js
+++ backend/heroku-test/server/index.js
@@ -3,6 +3,13 @@ const express = require("express");
 const { spawn } = require('child_process');
 const app = express();
 
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({ region: 'us-west-1' });
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
 // I HAVE DEFATED CORS
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://fencing-ai-ref.vercel.app"); // update to match the domain you will make the request from
@@ -14,25 +21,8 @@ app.use(express.json());
 const players = require('../data.json')
 
 app.get('/', (req, res) => {
-    // res.send('Hello World! I am changing')
-    console.log("home get request");
-
-    // Python testing here
-    var dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn('python', ['python/script2.py', 'aids', 'monkey']);
-    // collect data from script
-    python.stdout.on('data', data => {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        res.send(dataToSend)
-    });
-    // Python ending
+    console.log(`Here's the console for the server`);
+    res.send('Hello World! This is the node.js server')
 })
 
 app.get('/players/:id', (req, res) => {
@@ -60,22 +50,40 @@ app.get('/players', (req, res) => {
     res.send(players)
 })
 
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({ region: 'us-east-1' });
+app.get('/python', (req, res) => {
+    // Python testing here
+    var dataToSend;
+    // spawn new child process to call the python script
+    const python = spawn('python', ['python/script2.py', 'aids', 'monkey']);
+    // collect data from script
+    python.stdout.on('data', data => {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        // send data to browser
+        res.send(dataToSend)
+    });
+    // Python ending
+})
 
-// Create S3 service object
-s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+app.get('/aws', (req, res) => {
+    // Call S3 to list the buckets
+    s3.listBuckets(function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data.Buckets);
+            console.log(`First bucket: ${data.Buckets[0].Name} created at: ${data.Buckets[0].CreationDate}`);
+            bucketName = data.Buckets[0].Name
+            res.send(`First bucket: ${bucketName}`)
+        }
+    });
+})
 
-// Call S3 to list the buckets
-s3.listBuckets(function (err, data) {
-    if (err) {
-        console.log("Error", err);
-    } else {
-        console.log("Success", data.Buckets);
-    }
-});
+
 
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
